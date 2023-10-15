ed404266b3cfce6c0a003f3759091f0b2f174708
vxnzala1 <i02vesea@uco.es>
2023-10-15 23:54:52 +0200

    Actualización de diseño

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
 const githubUser = 'vxnzala1';
-
-/*fetch(`https://api.github.com/users/${githubUser}/repos`)
-  .then(response => response.json())
-  .then(data => {
-    let repos = "<ul>";
-    data.forEach(repo => {
-      repos += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
-    });
-    repos += "</ul>";
-    document.getElementById("repos").innerHTML = repos;
-  })
-  .catch(error => console.error('Error al obtener repositorios:', error));
-*/
